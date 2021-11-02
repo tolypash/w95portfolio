26dbdaf34a2a6ec7c27e890b82e2521408243fce
Anatoly <anatoly.pashias6@gmail.com>
2021-11-02 11:52:50 +0200

    game endpoints added

--- functions/src/games/tictactoe/utils.ts
+++ functions/src/games/tictactoe/utils.ts
@@ -13,3 +13,13 @@ export function create2DArray(x: number, y: number) {
 
     return result;
 }
+
+export function makeRandomID(length: number) {
+    const result = [];
+    const characters = '0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result.push(characters.charAt(Math.floor(Math.random() * charactersLength)));
+    }
+    return result.join('');
+}
